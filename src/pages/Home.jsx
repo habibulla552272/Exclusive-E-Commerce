40a6d75950dc9d85b26e5ad1222b0876c7017c76
habibulla552272 <mdhabibulla146425@gmail.com>
2025-02-02 21:35:15 +0600

    init tanstac query

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Component/Navbar/Navbar'
+import TestTanStack from '../Component/TanStack/TestTanStack'
 
 const Home = () => {
   return (
     <div>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit quis quaerat ipsam dolorum dolor excepturi nisi officiis corporis ut expedita. Reprehenderit placeat soluta molestias asperiores id facilis ipsam suscipit fuga.</p>
+      <TestTanStack />
     </div>
   )
 }
